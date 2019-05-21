690a99ecdf9eb4bf771887fe3d588e81b3b4e332
annazetterstrom <anna.wiktoria@me.com>
2019-05-21 10:50:06 +0200

    entry

--- src/classes/Entry.php
+++ src/classes/Entry.php
@@ -1,5 +1,12 @@
 <?php
 
+
 class Entry extends Mapper { 
-    
+    public function getEntrybyID($entryID){
+        $statement = $this->db->prepare("SELECT * FROM entries WHERE entryID = :entryID");
+        $statement->execute([
+            'entryID' => $entryID 
+        ]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
 }
