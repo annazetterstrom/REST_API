69e7627b1a6579a35014043e3b1022f9d3843148
annazetterstrom <anna.wiktoria@me.com>
2019-05-23 15:11:20 +0200

    entry

--- src/classes/Comment.php
+++ src/classes/Comment.php
@@ -1,27 +1,27 @@
 <?php
 
 class Comment extends Mapper {
-        public function getCommentByID($commentID){
-            $statement = $this->db->prepare("SELECT * FROM comments WHERE commentID = :commentID");
-            $statement->execute([
-                ':commentID' => $commentID 
-            ]);
+    public function getCommentByID($commentID){
+        $statement = $this->db->prepare("SELECT * FROM comments WHERE commentID = :commentID");
+        $statement->execute([
+            ':commentID' => $commentID 
+        ]);
             return $statement->fetch(PDO::FETCH_ASSOC);
         }
 
-        public function updateCommentByID($commentID){
-            $statement = $this->db->prepare("SELECT comments SET content=:content WHERE commentID = :commentID");
-            $statement->execute([
-                '' => $commentID
-            ]);
+    public function updateCommentByID($commentID){
+        $statement = $this->db->prepare("SELECT comments SET content=:content WHERE commentID = :commentID");
+        $statement->execute([
+            '' => $commentID
+        ]);
             
         }
 
-        public function deleteCommentsByID($commentID){
-            $statement = $this->db->prepare("DELETE FROM comments WHERE commentID = :commentID");
-            $statement->execute([
-                'commentID' => $commentID
-            ]);
+    public function deleteCommentsByID($commentID){
+        $statement = $this->db->prepare("DELETE FROM comments WHERE commentID = :commentID");
+        $statement->execute([
+            'commentID' => $commentID
+        ]);
             
         }
     
