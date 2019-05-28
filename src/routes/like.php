f9c7295685b2fdeabf21f74695dc9184b7727305
Tengil91 <kachiun91@gmail.com>
2019-05-28 11:46:35 +0200

    .

--- src/routes/like.php
+++ src/routes/like.php
@@ -4,7 +4,8 @@ return function ($app) {
     // Register auth middleware
   $auth = require __DIR__ . '/../middlewares/auth.php';
   
-  $app->post('/api/like', function ($request, $response) {
+  $app->post('/api/like/{id}', function ($request, $response) {
+    $entryID = $args['id'];
     $data = $request->getParsedBody();
     $like = new Likes($this->db);
     return $response->withJson($like->insertLike($data['entryID']));
